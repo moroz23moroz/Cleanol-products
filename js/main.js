03eb0280dfbdeaec72e727aa53b8f7e5a854cf41
Ilvina <ilvrakh@gmail.com>
2023-03-05 23:13:24 +0500

    styling block products

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const getData = () => {
             displayCards(data);
         })
         .catch(err => {
-            console.err(err)
+            console.log(err)
         });
 };
 
@@ -28,7 +28,9 @@ const displayCards = (data) => {
     for (let i = 0; i < 4; i++) {
 
     const productCard = document.createElement('div');
+    const pictureWrapper = document.createElement('div');
     const pictureCard = document.createElement('img');
+    const cardContent = document.createElement('div');
     const titleCard = document.createElement('h4');
     const infoText = document.createElement('p');
     const ingredientsWrapper = document.createElement('div');
@@ -38,10 +40,10 @@ const displayCards = (data) => {
     const sizeTitle = document.createElement('p');
     const size1 = document.createElement('div');
     const size2 = document.createElement('div');
-    const size3 = document.createElement('div');
     const buyButton = document.createElement('button');
-
     productCard.classList.add('products__product-card');
+    pictureWrapper.classList.add('products__card-img');
+    cardContent.classList.add('products__card-content')
     titleCard.classList.add('products__card-title');
     infoText.classList.add('products__card-info');
     ingredientsWrapper.classList.add('products__ingredients');
@@ -51,14 +53,17 @@ const displayCards = (data) => {
     sizeTitle.classList.add('products__card-subtitle');
     size1.classList.add('products__size-option');
     size2.classList.add('products__size-option');
-    size3.classList.add('products__size-option');
     buyButton.classList.add('products__button-buy');
 
-    productCard.append(pictureCard);
-    productCard.append(titleCard);
-    productCard.append(infoText);
-    productCard.append(ingredientsWrapper);
-    productCard.append(sizeWrapper);
+    productCard.append(pictureWrapper);
+    productCard.append(cardContent);
+
+    pictureWrapper.append(pictureCard);
+
+    cardContent.append(titleCard);
+    cardContent.append(infoText);
+    cardContent.append(ingredientsWrapper);
+    cardContent.append(sizeWrapper);
 
     ingredientsWrapper.append(subtitleText);
     ingredientsWrapper.append(listText);
@@ -66,7 +71,6 @@ const displayCards = (data) => {
     sizeWrapper.append(sizeTitle);
     sizeWrapper.append(size1);
     sizeWrapper.append(size2);
-    sizeWrapper.append(size3);
     sizeWrapper.append(buyButton);
 
     pictureCard.src = data[i].img;
@@ -77,8 +81,14 @@ const displayCards = (data) => {
     sizeTitle.textContent = 'Фасовка:';
     size1.textContent = data[i].size1;
     size2.textContent = data[i].size2;
-    size3.textContent = data[i].size3;
     buyButton.textContent = 'Купить';
+
+    if (data[i].size3) {
+        const size3 = document.createElement('div');
+        size3.classList.add('products__size-option');
+        sizeWrapper.append(size3);
+        size3.textContent = data[i].size3;
+        }
 
     document.querySelector('.products__visible-wrapper').append(productCard);
     }
@@ -88,7 +98,10 @@ document.addEventListener("DOMContentLoaded", getData());
 
 
 // <!-- <div class="products__product-card">
-//     <img src="./images/block4_products" alt="">
+//     <div class="products__card-img">
+//         <img src="./images/block4_products" alt="">
+//     </div> 
+// <div class="products__card-content">
 //     <h4 class="products__card-title"></h4>
 //     <p class="products__card-info"></p>
 //     <div class="products__ingredients">
@@ -97,9 +110,10 @@ document.addEventListener("DOMContentLoaded", getData());
 //     </div>
 //     <div class="products__size">
 //         <p class="products__card-subtitle">Фасовка:</p>
-//         <div class="products__size-option">1 л</div>
-//         <div class="products__size-option">5 кг</div>
-//         <div class="products__size-option">20 кг</div>
+//         <div class="products__button-size">1 л</div>
+//         <div class="products__button-size">5 кг</div>
+//         <div class="products__button-size">20 кг</div>
 //         <button class="products__button-buy">Купить</button>
 //     </div>
+// </div>
 // </div> -->
