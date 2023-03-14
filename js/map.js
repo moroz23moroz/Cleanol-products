292a4ec7dce5ddf4962f7b78ad9f5ea40917fbd1
MZ-87 <zsmile@mail.ru>
2023-03-14 16:14:51 +0300

    map style update

--- js/map.js
+++ js/map.js
@@ -9,56 +9,15 @@ ymaps.ready(function () {
     MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
         ),
-        console.log(MyIconContentLayout);
-    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-        hintContent: 'Собственный значок метки',
-        // balloonContent: 'Это красивая метка'
-    }, {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        // Своё изображение иконки метки.
-        iconImageHref: 'images/map_icon.svg',
-        // Размеры метки.
-        iconImageSize: [42, 55],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-5, -38]
-    });
 
-    //     var control = myMap.controls.get('routePanelControl');
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Cleanol, г. Минск, ул. Толбухина 2а, к. 320',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'images/map_icon.svg',
+            iconImageSize: [42, 55],
+            iconImageOffset: [-5, -38]
+        });
 
-    // control.routePanel.state.set({
-    //     type: 'masstransit',
-    //     fromEnabled: true,
-    //     toEnabled: false,
-    //     to: 'Москва, Погорельский переулок 6',
-    // });
-
-    // control.routePanel.options.set({
-    //     allowSwitch: false,
-    //     reverseGeocoding: true,
-    //     types: {
-    //         masstransit: true,
-    //         pedestrian: true,
-    //         taxi: true
-    //     }
-    // });
-
-    // var switchPointsButton = new ymaps.control.Button({
-    //     data: {
-    //         content: "Поменять местами",
-    //         title: "Поменять точки местами"
-    //     },
-    //     options: {
-    //         selectOnClick: false,
-    //         maxWidth: 160
-    //     }
-    // });
-
-    // switchPointsButton.events.add('click', function () {
-    //     control.routePanel.switchPoints();
-    // });
-    // myMap.controls.add(switchPointsButton);
-
+        myMap.geoObjects.add(myPlacemark);
 });
