bbe56c1d09852b4ae0eb0e18250da77fd914cc5c
Marina Moroz <moroz2387moroz@yandex.ru>
2023-03-24 16:50:17 +1000

    fix bugs of adaptive-menu

--- js/popup.js
+++ js/popup.js
@@ -6,4 +6,12 @@ $(document).ready(function () {
     $('.popup__close').on('click', function () {
         $('#popup').removeClass('open');
     });
+
+    $('.icon-menu').on('click', function () {
+        $('.adaptive-menu').addClass('action');
+    });
+    $('.adaptive-menu__close').on('click', function () {
+        $('.adaptive-menu').removeClass('action');
+    });
+
 });
